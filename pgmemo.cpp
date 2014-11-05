277d58ee454c3d70dc66cf21e9caf08c4c8cbabf
Nathan Eloe <powerofazure@gmail.com>
2014-11-05 10:14:54 -0600

    Dynamically create the required protobuf

---
+++ pgmemo.cpp
@@ -0,0 +1,14 @@
+/*!
+ * \file pgmemo.cpp
+ * \author Nathan Eloe
+ * \brief Driver for PGMemo
+ */
+
+#include <iostream>
+#include "pgmemo.pb.h"
+
+int main()
+{
+  PGMemoRequest pgmr;
+  return 0;
+}
