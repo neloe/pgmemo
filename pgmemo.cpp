797cab9ba905c2895794aaf4ccffd3448a96609f
Nathan Eloe <powerofazure@gmail.com>
2014-11-07 09:28:03 -0600

    a bit of a cleanup

--- pgmemo.cpp
+++ pgmemo.cpp
@@ -37,21 +37,5 @@ int main(int argc, char* argv[])
     if(pgmr.cached() && pgmr.refresh())
       update_cache(pgmr, pgmconf);
   }
-  /*
-  pgmr.set_dbname("postgres");
-  pgmr.set_query("select clock_timestamp();");
-  std::cout << pgmr.cached() << std::endl;
-  memo_query(pgmr, pgmconf);
-  std::cout << pgmr.result_json() << " " << pgmr.cached() << std::endl;
-  pgmr1.set_dbname("postgres");
-  pgmr1.set_query("select clock_timestamp();");
-  pgmr1.set_refresh(true);
-  std::cout << pgmr1.cached() << std::endl;
-  memo_query(pgmr1, pgmconf);
-  std::cout << pgmr1.result_json() << " " << pgmr1.cached() << std::endl;
-  if (pgmr1.cached() && pgmr1.refresh())
-    update_cache(pgmr1, pgmconf);
-  std::cout << pgmr1.result_json() << " " << pgmr1.cached() << std::endl;
-  */
   return 0;
 }
